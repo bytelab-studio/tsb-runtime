66988d708e26c696dc79544cb2a58b206c4d3cf0
Christoph Koschel <christoph.koschel86@gmail.com>
2024-08-24 22:41:23 +0200

    Merge header files into one

--- types/index.d.ts
+++ types/index.d.ts
@@ -48,4 +48,31 @@ declare global {
     const AppDomain: TSB.AppDomain;
 }
 
-export * from "./tsb";
+declare module "tsb" {
+    type TSBPlatform = "nodejs" | "browser" | string;
+    type TSBPlugin = "minify" | "macro" | string;
+
+    class Builder {
+        public module(name: string): this ;
+
+        public platform(platform: TSBPlatform): this;
+
+        public entry(file: string): this;
+
+        public output(folder: string): this;
+
+        public addFiles(...files: string[]): this;
+
+        public addFolders(...folders: string[]): this;
+
+        public chunkSize(size: number): this;
+
+        public embeddedFileMap(value: boolean): this;
+
+        public plugins(...names: TSBPlugin[]): this;
+    }
+
+    export const builder: Builder;
+}
+
+export {};
